dc4e8266e6123d4a97a8b2e006f08591a857ebac
Miroslav Mirchev <mirchev1977@yahoo.com>
2022-03-23 04:05:33 +0200

    cerated the auth logic for registration, login and logout

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,12 @@
 import { createStore } from 'vuex';
 
 import tutorsModule from './modules/tutors/index.js';
+import authModule from './modules/auth/index.js';
 
 const store = createStore({
     modules: {
         tutors: tutorsModule,
+        auth: authModule
     }
 });
 
