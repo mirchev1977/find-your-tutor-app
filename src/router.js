9fd58065854f798f7e1599802030938e410ed7f3
Miroslav Mirchev <mirchev1977@yahoo.com>
2022-03-23 04:42:17 +0200

    implemented NotFound.vue

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 
 import TutorsList from './pages/tutors/TutorsList.vue';
 import TutorRegistation from './pages/tutors/TutorRegistration.vue';
+import NotFound from './pages/NotFound.vue';
 import UserAuth from './pages/auth/UserAuth.vue';
 
 
@@ -12,6 +13,7 @@ const router = createRouter({
         { path: '/tutors', component: TutorsList },
         { path: '/register', component: TutorRegistation, meta: { requiresAuth: true } },
         { path: '/auth', component: UserAuth, meta: { requiresUnauth: true } },
+        { path: '/:notFound(.*)', component: NotFound }
     ]
 });
 
