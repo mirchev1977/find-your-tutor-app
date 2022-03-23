b42f999698bee7c05e9c744213310fd83bf6d414
Miroslav Mirchev <mirchev1977@yahoo.com>
2022-03-23 04:49:54 +0200

    implemented TutorDetail.vue

--- src/router.js
+++ src/router.js
@@ -1,7 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
+import TutorDetail from './pages/tutors/TutorDetail.vue';
 import TutorsList from './pages/tutors/TutorsList.vue';
 import TutorRegistation from './pages/tutors/TutorRegistration.vue';
+import ContactTutor from './pages/inquiries/ContactTutor.vue';
 import NotFound from './pages/NotFound.vue';
 import UserAuth from './pages/auth/UserAuth.vue';
 
@@ -11,6 +13,14 @@ const router = createRouter({
     routes: [
         { path: '/', redirect: '/tutors' },
         { path: '/tutors', component: TutorsList },
+        {
+          path: '/tutors/:id',
+          component: TutorDetail,
+          props: true,
+          children: [
+            { path: 'contact', component: ContactTutor } // /tutors/c1/contact
+          ]
+        },
         { path: '/register', component: TutorRegistation, meta: { requiresAuth: true } },
         { path: '/auth', component: UserAuth, meta: { requiresUnauth: true } },
         { path: '/:notFound(.*)', component: NotFound }
